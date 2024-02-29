1cf210680880077be9b61961ef57c17fdbb555f4
Gabriel Velez <gabriel.velez131@gmail.com>
2024-02-28 22:40:04 -0400

    Fixing console

--- console.py
+++ console.py
@@ -8,18 +8,13 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
-    def do_help(self, arg: str):
-        """Shows documentation of the options\n"""
-        return super().do_help(arg)
-
-    def do_quit(self, arg: str):
+    def do_quit(self, arg):
         """Quit command to exit the program\n"""
         return True
 
-    def do_EOF(self, arg: str):
+    def do_EOF(self, arg):
         """Quit command to exit the program\n"""
         return True
-
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
