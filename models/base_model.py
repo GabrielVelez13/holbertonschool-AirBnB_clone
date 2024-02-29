493c80fed114b36a3fa812b8b265a45d034dcd3d
Gabriel Velez <gabriel.velez131@gmail.com>
2024-02-28 23:37:19 -0400

    fixing basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,6 @@
 """Defining the basemodel of airbnb clone"""
 from uuid import uuid4
 from datetime import datetime
-import models
-
 
 class BaseModel:
     """ BaseModel class """
@@ -13,15 +11,6 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                time_format = "%Y-%m-%dT%H:%M:%S.%f"
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, time_format)
-                else:
-                    self.__dict__[key] = value
-        else:
-            models.storage.new(self)
 
     def __str__(self) -> str:
         """ Adds to the str. """
@@ -31,7 +20,6 @@ class BaseModel:
     def save(self):
         """ Saves the data. """
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """ Sets extra keys in the dictionary. """
