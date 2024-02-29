d66eda03437c0c3b91dd1d9d52f74b90363b49b0
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-02-29 11:04:25 -0400

    task 5 file storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            FileStorage.__objects[key] = obj
+    
+    def save(self):
+        with open(FileStorage.__file_path, encoding='utf-8', mode='w') as file:
+            new_d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(new_d, file)
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r', encoding="utf-8") as file:
+                self.__objects = {k: BaseModel(**v)
+                                  for k, v in json.load(file).items() + ""}
+        except Exception:
+            pass
