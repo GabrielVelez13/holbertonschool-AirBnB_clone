6712d87c5f73ba7a9489a1f2ed9d2142bd4cfd75
Leandro Pagani <143301555+LeaPagani@users.noreply.github.com>
2024-03-01 21:12:38 -0400

    fixing issues with console

--- console.py
+++ console.py
@@ -16,15 +16,40 @@ class HBNBCommand(cmd.Cmd):
     """Console for Airbnb clone."""
 
     prompt = "(hbnb) "
-    class_dict = {
-        "BaseModel": BaseModel,
-        "Amenity": Amenity,
-        "City": City,
-        "Place": Place,
-        "User": User,
-        "Review": Review,
-        "State": State
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
     }
+
+    def emptyline(self):
+        """Does nothing when nothing is entered"""
+        pass
+
+    def default(self, arg):
+        """Default behavior for cmd module when input is invalid"""
+        argdict = {
+            "all": self.do_all,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "count": self.do_count,
+            "update": self.do_update
+        }
+        match = re.search(r"\.", arg)
+        if match is not None:
+            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            match = re.search(r"\((.*?)\)", argl[1])
+            if match is not None:
+                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
+                if command[0] in argdict.keys():
+                    call = "{} {}".format(argl[0], command[1])
+                    return argdict[command[0]](call)
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
     def do_quit(self, arg):
         """Quit command to exit the program\n"""
@@ -33,10 +58,6 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, arg):
         """Quit command to exit the program\n"""
         return True
-
-    def emptyline(self):
-        """Does nothing when nothing is entered"""
-        pass
 
     def do_create(self, arg):
         if not arg:
